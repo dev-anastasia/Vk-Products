957e3c1932c48e5c0a6ce3b4f4415b15c1619046
Anastasia Stepanova <stepanovawm@gmail.com>
2024-03-12 22:01:52 +0300

    Final fixes. Ready to go!

--- app/src/main/java/com/example/vk_products_app/entities/Product.kt
+++ app/src/main/java/com/example/vk_products_app/entities/Product.kt
@@ -1,8 +1,5 @@
 package com.example.vk_products_app.entities
 
-import android.os.Parcel
-import android.os.Parcelable
-
 data class Product(
     val id: Int,
     val title: String,
@@ -12,42 +9,4 @@ data class Product(
     val brand: String,
     val thumbnail: String,
     val images: List<String>
-) : Parcelable {
-
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readString()?: "",
-        parcel.readString()?: "",
-        parcel.readInt(),
-        parcel.readDouble(),
-        parcel.readString()?: "",
-        parcel.readString()?: "",
-        parcel.createStringArrayList()?: emptyList()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(id)
-        parcel.writeString(title)
-        parcel.writeString(description)
-        parcel.writeInt(price)
-        parcel.writeDouble(rating)
-        parcel.writeString(brand)
-        parcel.writeString(thumbnail)
-        parcel.writeStringList(images)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Product> {
-        override fun createFromParcel(parcel: Parcel): Product {
-            return Product(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Product?> {
-            return arrayOfNulls(size)
-        }
-    }
-
-}
+)
